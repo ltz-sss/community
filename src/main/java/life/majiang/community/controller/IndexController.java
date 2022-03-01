23b6b3fd9d0cf42ac985d8ecba96ddd711410d73
ltz-sss <2808202262@qq.com>
2022-03-01 20:11:57 +0800

    extract github config from code to properties

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class StartController {
+public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
